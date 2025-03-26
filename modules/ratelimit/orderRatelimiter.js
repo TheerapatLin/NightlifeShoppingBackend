3e42c8c33790771d12287797bc47597de6849139
tjnut3 <notakcoblet@gmail.com>
2025-03-26 19:16:24 +0700

    UpdateOrderRatelimiter

--- modules/ratelimit/orderRatelimiter.js
+++ modules/ratelimit/orderRatelimiter.js
@@ -5,65 +5,44 @@ const RedisStore = require('rate-limit-redis');
 const redis = require('../database/redis');
 
 const createOrderRateLimiter = rateLimit({
-    /*store: new RedisStore({
-        sendCommand: (...args) => redis.sendCommand(args),
-    }),
-    */
+    keyGenerator: (req) => {
+        const ip = req.ip || req.headers['x-forwarded-for'] ||  req.connection.remoteAddress; // รับค่า IP จาก User
+        console.log("Request IP:", ip);  // ตรวจสอบว่าได้ค่า IP หรือไม่
+        return ip;
+    },
     windowMs: 10 * 60 * 1000, // 10 minutes
-    max: 1000, // limit each IP to 5 requests per window Ms
-    standardHeaders: true,
-    legacyHeaders: false,
-    message: "Exceeded Rate Limit, please try again in 10 minutes"
-});
-
-const getOrderRateLimiter = rateLimit({
-    /*store: new RedisStore({
-        sendCommand: (...args) => redis.sendCommand(args),
-    }),
-    */
-    windowMs: 10 * 60 * 1000, // 10 minutes
-    max: 1000, // limit each IP to 5 requests per window Ms
+    max: 10, // limit each IP to 5 requests per window Ms
     standardHeaders: true,
     legacyHeaders: false,
     message: "Exceeded Rate Limit, please try again in 10 minutes"
 });
 
 const getOrdersRateLimiter = rateLimit({
-    /*store: new RedisStore({
-        sendCommand: (...args) => redis.sendCommand(args),
-    }),
-    */
+    keyGenerator: (req) => {
+        const ip = req.ip || req.headers['x-forwarded-for'] ||  req.connection.remoteAddress; // รับค่า IP จาก User
+        console.log("Request IP:", ip);  // ตรวจสอบว่าได้ค่า IP หรือไม่
+        return ip;
+    },
     windowMs: 10 * 60 * 1000, // 10 minutes
-    max: 1000, // limit each IP to 5 requests per window Ms
-    standardHeaders: true,
-    legacyHeaders: false,
-    message: "Exceeded Rate Limit, please try again in 10 minutes"
-});
-
-const deleteOrderRateLimiter = rateLimit({
-    /*store: new RedisStore({
-        sendCommand: (...args) => redis.sendCommand(args),
-    }),
-    */
-    windowMs: 10 * 60 * 1000, // 10 minutes
-    max: 1000, // limit each IP to 3 requests per window Ms
+    max: 10, // limit each IP to 5 requests per window Ms
     standardHeaders: true,
     legacyHeaders: false,
     message: "Exceeded Rate Limit, please try again in 10 minutes"
 });
 
 const deleteOrdersRateLimiter = rateLimit({
-    /*store: new RedisStore({
-        sendCommand: (...args) => redis.sendCommand(args),
-    }),
-    */
+    keyGenerator: (req) => {
+        const ip = req.ip || req.headers['x-forwarded-for'] ||  req.connection.remoteAddress; // รับค่า IP จาก User
+        console.log("Request IP:", ip);  // ตรวจสอบว่าได้ค่า IP หรือไม่
+        return ip;
+    },
     windowMs: 10 * 60 * 1000, // 10 minutes
-    max: 1000, // limit each IP to 3 requests per window Ms
+    max: 10, // limit each IP to 3 requests per window Ms
     standardHeaders: true,
     legacyHeaders: false,
     message: "Exceeded Rate Limit, please try again in 10 minutes"
 });
 
 module.exports = {
-    createOrderRateLimiter, getOrderRateLimiter, getOrdersRateLimiter, deleteOrderRateLimiter, deleteOrdersRateLimiter
+    createOrderRateLimiter, getOrdersRateLimiter, deleteOrdersRateLimiter
 }
