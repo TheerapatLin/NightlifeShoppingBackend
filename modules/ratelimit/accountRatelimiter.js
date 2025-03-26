a2cd697da41ab845f3c169f8194fa2dc8b7e4515
tgtbus25277 <byf.2545@gmail.com>
2025-03-26 21:20:22 +0700

    update ratelimiter - authRatelimiter  Registers, Login, Logout - accountRatelimiter  GetAccounts , DeleteAccounts - authRatelimiter  add logoutRateLimiter and loginRateLimiter

--- modules/ratelimit/accountRatelimiter.js
+++ modules/ratelimit/accountRatelimiter.js
@@ -17,53 +17,73 @@ const abcRateLimiter = rateLimit({
 });
 
 
-const getAccountRateLimiter = rateLimit({
-    store: new RedisStore({
-        sendCommand: (...args) => redis.sendCommand(args),
-    }),
-    windowMs: 10 * 60 * 1000, // 10 minutes
-    max: 1000, // limit each IP to 5 requests per window Ms
-    standardHeaders: true,
-    legacyHeaders: false,
-    message: "Exceeded Rate Limit, please try again in 10 minutes"
-});
-
 const getAccountsRateLimiter = rateLimit({
     /*store: new RedisStore({
         sendCommand: (...args) => redis.sendCommand(args),
     }),
     */
+    keyGenerator: (req) => {
+        const ip = req.ip || req.headers['x-forwarded-for'] || req.connection.remoteAddress; // รับค่า IP จาก User
+        console.log("Request IP:", ip);  // ตรวจสอบว่าได้ค่า IP หรือไม่
+        return ip;
+    },
     windowMs: 10 * 60 * 1000, // 10 minutes
-    max: 1000, // limit each IP to 5 requests per window Ms
+    max: 100, // limit each IP to 5 requests per window Ms
+    standardHeaders: true,
+    legacyHeaders: false,
+    message: "Exceeded Rate Limit, please try again in 10 minutes"
+});
+const getAccountRateLimiter = rateLimit({
+    /*store: new RedisStore({
+        sendCommand: (...args) => redis.sendCommand(args),
+    }),
+    */
+    keyGenerator: (req) => {
+        const ip = req.ip || req.headers['x-forwarded-for'] || req.connection.remoteAddress; // รับค่า IP จาก User
+        console.log("Request IP:", ip);  // ตรวจสอบว่าได้ค่า IP หรือไม่
+        return ip;
+    },
+    windowMs: 10 * 60 * 1000, // 10 minutes
+    max: 100, // limit each IP to 5 requests per window Ms
     standardHeaders: true,
     legacyHeaders: false,
     message: "Exceeded Rate Limit, please try again in 10 minutes"
 });
 
-const deleteAccountRateLimiter = rateLimit({
-    /*store: new RedisStore({
-        sendCommand: (...args) => redis.sendCommand(args),
-    }),
-    */
-    windowMs: 10 * 60 * 1000, // 10 minutes
-    max: 1000, // limit each IP to 3 requests per window Ms
-    standardHeaders: true,
-    legacyHeaders: false,
-    message: "Exceeded Rate Limit, please try again in 10 minutes"
-});
 
 const deleteAccountsRateLimiter = rateLimit({
     /*store: new RedisStore({
         sendCommand: (...args) => redis.sendCommand(args),
     }),
     */
+    keyGenerator: (req) => {
+        const ip = req.ip || req.headers['x-forwarded-for'] || req.connection.remoteAddress; // รับค่า IP จาก User
+        console.log("Request IP:", ip);  // ตรวจสอบว่าได้ค่า IP หรือไม่
+        return ip;
+    },
     windowMs: 10 * 60 * 1000, // 10 minutes
-    max: 1000, // limit each IP to 3 requests per window Ms
+    max: 100, // limit each IP to 3 requests per window Ms
+    standardHeaders: true,
+    legacyHeaders: false,
+    message: "Exceeded Rate Limit, please try again in 10 minutes"
+});
+const deleteAccountRateLimiter = rateLimit({
+    /*store: new RedisStore({
+        sendCommand: (...args) => redis.sendCommand(args),
+    }),
+    */
+    keyGenerator: (req) => {
+        const ip = req.ip || req.headers['x-forwarded-for'] || req.connection.remoteAddress; // รับค่า IP จาก User
+        console.log("Request IP:", ip);  // ตรวจสอบว่าได้ค่า IP หรือไม่
+        return ip;
+    },
+    windowMs: 10 * 60 * 1000, // 10 minutes
+    max: 100, // limit each IP to 3 requests per window Ms
     standardHeaders: true,
     legacyHeaders: false,
     message: "Exceeded Rate Limit, please try again in 10 minutes"
 });
 
 module.exports = {
-    abcRateLimiter, getAccountRateLimiter, getAccountsRateLimiter, deleteAccountRateLimiter, deleteAccountsRateLimiter
+    abcRateLimiter, getAccountsRateLimiter, deleteAccountsRateLimiter, getAccountRateLimiter, deleteAccountRateLimiter
 }
