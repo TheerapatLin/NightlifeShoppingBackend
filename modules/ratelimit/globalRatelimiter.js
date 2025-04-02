0307073ee5ea46cc3cf6ac4c968fd23dc4035be4
tjnut3 <notakcoblet@gmail.com>
2025-04-03 02:31:59 +0700

    Refactoring venueRoutes, venueController

--- modules/ratelimit/globalRatelimiter.js
+++ modules/ratelimit/globalRatelimiter.js
@@ -5,12 +5,13 @@ const RedisStore = require('rate-limit-redis');
 const redis = require('../database/redis');
 
 const globalRateLimit = rateLimit({
-    /*store: new RedisStore({
-        sendCommand: (...args) => redis.sendCommand(args),
-    }),
-    */
+    keyGenerator: (req) => {
+        const ip = req.ip || req.headers['x-forwarded-for'] ||  req.connection.remoteAddress; // รับค่า IP จาก User
+        console.log("Request IP:", ip);  // ตรวจสอบว่าได้ค่า IP หรือไม่
+        return ip;
+    },
     windowMs: 5 * 60 * 1000, // 5 minutes
-    max: 1000, // limit each IP to 50 requests per window Ms
+    max: 100, // limit each IP to 50 requests per window Ms
     skipFailedRequests: true,
     standardHeaders: true,
     legacyHeaders: false,
