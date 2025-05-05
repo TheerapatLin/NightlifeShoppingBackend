516c1bd6d05f91897f8ee505a7496c94ba42de87
siriake <siri.ake.1980@gmail.com>
2025-05-05 20:32:02 +0700

    1

--- routes/v1/indexRoutes.js
+++ routes/v1/indexRoutes.js
@@ -6,7 +6,7 @@ require('dotenv').config({ path: `.env.${process.env.NODE_ENV}` });
 router.get("/", function (req, res, next) {
   res.render("index", {
     pageTitle: "Hiddengem Tech UAT-API : Endpoints",
-    featuredContent: "version 1.0.3 (build 10)",
+    featuredContent: "version 1.0.4 (build 11)",
     environment: process.env.NODE_ENV
   });
 });
